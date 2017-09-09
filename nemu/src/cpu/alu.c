bb146848880e4ce4aa7b76c06bf81334885cae17
tracer-pa2017 <tracer@njuics.org>
2017-09-09 17:10:19 +0800

    >  nemu 161220066 lgy Linux Heisenberg 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  17:10:19 up  2:29,  1 user,  load average: 0.08, 0.04, 0.00 c0172b82d7c091ac148c96c58ea11db0c2251a57

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -72,7 +72,8 @@ uint32_t alu_add(uint32_t src, uint32_t dest) {
 void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest)
 {
     unsigned temp=result<src||result<(dest+cpu.eflags.CF)||result<(src+cpu.eflags.CF)||result<dest;
-    cpu.eflags.CF=temp^cpu.eflags.CF;
+   // cpu.eflags.CF=temp^cpu.eflags.CF;
+   cpu.eflag.CF=temp;
 }
 
 
