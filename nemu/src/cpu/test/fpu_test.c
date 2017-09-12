88990f20da0e6cf3cd474a54149b5419e204a8fb
tracer-pa2017 <tracer@njuics.org>
2017-09-12 09:20:11 +0800

    >  nemu 161220066 lgy Linux Heisenberg 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  09:20:11 up 29 min,  1 user,  load average: 0.39, 0.25, 0.20 b2afa1a2cbdd56bbb7ede0c4bd09fcb6a4ab9239

--- nemu/src/cpu/test/fpu_test.c
+++ nemu/src/cpu/test/fpu_test.c
@@ -128,10 +128,10 @@ void fpu_test_mul() {
 			res.fval = a.fval * b.fval;
 			res_fpu.val = internal_float_mul(b.val, a.val);
 			printf("float mul a = %f, b = %f, ua = %x, ub = %x, res = %x, res_fpu = %x, res = %f, res_fpu = %f\n", a.fval, b.fval, a.val, b.val, res.val, res_fpu.val, res.fval, res_fpu.fval);
-			printf("a       sign %x, exp %x, %d, sig %x\n", a.sign, a.exponent, a.exponent, a.significand);
-			printf("b       sign %x, exp %x, %d, sig %x\n", b.sign, b.exponent, b.exponent, b.significand);
-			printf("res     sign %x, exp %x, %d, sig %x\n", res.sign, res.exponent, res.exponent, res.significand);
-			printf("res_fpu sign %x, exp %x, %d, sig %x\n", res_fpu.sign, res_fpu.exponent, res_fpu.exponent, res_fpu.significand);
+			printf("a       sign %x, exp %x, %d, sig %x\n", a.sign, a.exponent, a.exponent, a.fraction);
+			printf("b       sign %x, exp %x, %d, sig %x\n", b.sign, b.exponent, b.exponent, b.fraction);
+			printf("res     sign %x, exp %x, %d, sig %x\n", res.sign, res.exponent, res.exponent, res.fraction);
+			printf("res_fpu sign %x, exp %x, %d, sig %x\n", res_fpu.sign, res_fpu.exponent, res_fpu.exponent, res_fpu.fraction);
             
             if(res_fpu.val!=res.val)
             {
