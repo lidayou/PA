9ca4cbc2143d5d63db44fedebef1f1a479a00b0a
tracer-pa2017 <tracer@njuics.org>
2017-09-12 09:12:21 +0800

    >  nemu 161220066 lgy Linux Heisenberg 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  09:12:21 up 21 min,  1 user,  load average: 0.39, 0.25, 0.21 4cefcb8751c663387151929aecb5615c25accc29

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -273,8 +273,9 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a) {
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-	printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
-	assert(0);
+	
+    exp_res=fa.exponent+fb.exponent;
+
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
 
