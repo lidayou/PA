970ca605b4652fda4933b22caac1ed3b99908086
tracer-pa2017 <tracer@njuics.org>
2017-09-18 18:51:08 +0800

    >  nemu 161220066 lgy Linux Heisenberg 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  18:51:07 up 18 min,  1 user,  load average: 0.02, 0.18, 0.26 630dfd5bc658751de7adf9f9c21811319ea0cc4b

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -33,7 +33,7 @@ instr_func opcode_entry[256] = {
 /* 0x74 - 0x77*/	inv, inv, inv, inv,
 /* 0x78 - 0x7b*/	inv, inv, inv, inv,
 /* 0x7c - 0x7f*/	inv, inv, inv, inv,
-/* 0x80 - 0x83*/	group_1_b, group_1_v,break_point, group_1_bv,
+/* 0x80 - 0x83*/	group_1_b, group_1_v,nemu_trap, group_1_bv,
 /* 0x84 - 0x87*/	inv, inv, inv, inv,
 /* 0x88 - 0x8b*/	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
 /* 0x8c - 0x8f*/	inv, inv, inv, inv,
