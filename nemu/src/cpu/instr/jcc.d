183d3c58b7d4b8c8c1c94f2284b882c7a8ccd16c
tracer-pa2017 <tracer@njuics.org>
2017-09-19 21:56:15 +0800

    >  nemu 161220066 lgy Linux Heisenberg 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u3 (2017-08-06) i686 GNU/Linux  21:56:15 up  3:57,  1 user,  load average: 0.02, 0.10, 0.09 6c776aaa90889f7dfe584f667bc0b8eb4742a810

--- nemu/src/cpu/instr/jcc.d
+++ nemu/src/cpu/instr/jcc.d
@@ -7,7 +7,8 @@ src/cpu/instr/jcc.o: src/cpu/instr/jcc.c include/cpu/instr.h \
  include/cpu/instr_helper.h include/cpu/instr/mov.h \
  include/cpu/instr/jmp.h include/cpu/instr/shift.h \
  include/cpu/instr/flags.h include/cpu/instr/group.h \
- include/cpu/instr/special.h include/cpu/instr/x87.h
+ include/cpu/instr/special.h include/cpu/instr/x87.h \
+ include/cpu/instr/jcc.h
 
 include/cpu/instr.h:
 
@@ -54,3 +55,5 @@ include/cpu/instr/group.h:
 include/cpu/instr/special.h:
 
 include/cpu/instr/x87.h:
+
+include/cpu/instr/jcc.h:
